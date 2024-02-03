8c006fe6ce65554441a389cafc009b05baf2494c
bikashshaw <bikashvicky@gmail.com>
2024-02-03 11:53:26 +0530

    created upto video player

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -5,6 +5,7 @@ import { getCourses } from "@/actions/get-courses";
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { CoursesList } from "@/components/courses-list";
 
 interface SearchPageProps {
   searchParams: {
@@ -34,8 +35,9 @@ const SeacrhPage = async ({ searchParams }: SearchPageProps) => {
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
